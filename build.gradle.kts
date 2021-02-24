3d435ffdc69492a3368d8aed72e442a919b7037c
suyang <suyang@haohandata.com.cn>
2021-02-24 14:55:01 +0800

    init project

---
+++ build.gradle.kts
@@ -0,0 +1,67 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "2.4.2"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.4.21"
+    kotlin("plugin.spring") version "1.4.21"
+    kotlin("plugin.jpa") version "1.4.21"
+}
+
+allprojects {
+
+    apply {
+        plugin("java")
+        plugin("org.springframework.boot")
+        plugin("io.spring.dependency-management")
+        plugin("kotlin")
+        plugin("kotlin-spring")
+        plugin("kotlin-jpa")
+    }
+
+    group = "com.github.xsi640"
+    version = "0.0.1-SNAPSHOT"
+    java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+    val user = System.getProperty("repoUser")
+    val pwd = System.getProperty("repoPassword")
+
+    repositories {
+        mavenLocal()
+        maven {
+            credentials {
+                username = user
+                password = pwd
+            }
+            url = uri("http://192.168.1.254:8080/nexus/repository/maven2-group/")
+        }
+    }
+
+    val vers = mapOf(
+        "cglib" to "3.3.0",
+        "commons_io" to "2.6",
+        "commons_codec" to "1.15",
+        "commons_lang" to "3.9"
+    )
+
+    dependencies {
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "1.8"
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+rootProject.name = "rest-api-flow"
+
+fun defineSubProject(name: String, subdir: String = "") {
+    val prjName = "rest-api-flow-$name"
+    include(prjName)
+    if (subdir.isEmpty()) {
+        mkdir(prjName)
+        project(":$prjName").projectDir = file(prjName)
+    } else {
+        val dir = "$subdir/$prjName"
+        mkdir(dir)
+        project(":$prjName").projectDir = file(dir)
+    }
+}
+
+defineSubProject("core", "")
